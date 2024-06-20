2a2a6ad5061f5bdd0df9d3e8bb87fea5c2503c6b
heilviva <145266521+heilviva@users.noreply.github.com>
2024-06-20 18:37:28 +0300

    Update mod.java

--- mod.java
+++ mod.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class mod {
+package net.halikov.mptmod;
+
+import com.mojang.logging.LogUtils;
+import net.halikov.mptmod.block.ModBlocks;
+import net.halikov.mptmod.item.ModItems;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(mod.MOD_ID)
+@Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public static class ClientModEvents
+{
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event)
+    {
+
+    }
+}
+
+
+
+public class mod
+{
+
+    public static final String MOD_ID = "mptmod";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public mod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
 }
